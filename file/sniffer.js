4ec6a35b354116fdb568fdf7316b8e99176989bb
codedread <jeffschiller@google.com>
2020-03-19 09:22:19 -0700

    Add bz2 and pdf to file sniffer.  Allow multiple signatures per file type.

--- file/sniffer.js
+++ file/sniffer.js
@@ -11,15 +11,24 @@ bitjs.file = bitjs.file || {};
 
 (function() {
 
+// A selection from https://en.wikipedia.org/wiki/List_of_file_signatures.
+// Mapping of MIME type to magic numbers.  Each file type can have multiple signatures.
+// '??' is used as a placeholder value.
+// TODO:  Add audio/video formats?
 const fileSignatures = {
-  'application/x-7z-compressed': [0x37, 0x7A, 0xBC, 0xAF, 0x27, 0x1C],
-  'application/x-rar-compressed': [0x52, 0x61, 0x72, 0x21, 0x1A, 0x07],
-  'application/zip': [0x50, 0x4B],
-  'image/bmp': [0x42, 0x4D],
-  'image/gif': [0x47, 0x49, 0x46, 0x38],
-  'image/jpeg': [0xFF, 0xD8, 0xFF],
-  'image/png': [0x89, 0x50, 0x4E, 0x47, 0x0D, 0x0A, 0x1A, 0x0A],
-  'image/webp': [0x52, 0x49, 0x46, 0x46, '??', '??', '??', '??', 0x57, 0x45, 0x42, 0x50],
+  // Document formats.
+  'application/pdf': [[0x25, 0x50, 0x44, 0x46, 0x2d]],
+  // Compressed archive formats.
+  'application/x-7z-compressed': [[0x37, 0x7A, 0xBC, 0xAF, 0x27, 0x1C]],
+  'application/x-bzip2': [[0x42, 0x5A, 0x68]],
+  'application/x-rar-compressed': [[0x52, 0x61, 0x72, 0x21, 0x1A, 0x07]],
+  'application/zip': [[0x50, 0x4B, 0x03, 0x04], [0x50, 0x4B, 0x05, 0x06], [0x50, 0x4B, 0x07, 0x08]],
+  // Image formats.
+  'image/bmp': [[0x42, 0x4D]],
+  'image/gif': [[0x47, 0x49, 0x46, 0x38]],
+  'image/jpeg': [[0xFF, 0xD8, 0xFF]],
+  'image/png': [[0x89, 0x50, 0x4E, 0x47, 0x0D, 0x0A, 0x1A, 0x0A]],
+  'image/webp': [[0x52, 0x49, 0x46, 0x46, '??', '??', '??', '??', 0x57, 0x45, 0x42, 0x50]],
 };
 
 class Node {
@@ -37,30 +46,33 @@ let maxDepth = 0;
 
 // Construct the tree, erroring if overlapping mime types are possible.
 for (const mimeType in fileSignatures) {
-  let curNode = root;
-  let depth = 0;
-  for (const byte of fileSignatures[mimeType]) {
-    if (curNode.children[byte] === undefined) {
-      if (byte === '??' && !curNode.children['??'] && Object.keys(curNode.children).length > 0) {
-        throw 'Cannot add a placeholder child to a node that has non-placeholder children';
-      } else if (byte !== '??' && curNode.children['??']) {
-        throw 'Cannot add a non-placeholder child to a node that has a placeholder child';
+  for (const signature of fileSignatures[mimeType]) {
+    let curNode = root;
+    let depth = 0;
+    for (const byte of signature) {
+      if (curNode.children[byte] === undefined) {
+        if (byte === '??' && !curNode.children['??'] && Object.keys(curNode.children).length > 0) {
+          throw 'Cannot add a placeholder child to a node that has non-placeholder children';
+        } else if (byte !== '??' && curNode.children['??']) {
+          throw 'Cannot add a non-placeholder child to a node that has a placeholder child';
+        }
+        curNode.children[byte] = new Node(byte);
       }
-      curNode.children[byte] = new Node(byte);
-    }
-    depth++;
-    curNode = curNode.children[byte];
-  }
-  if (maxDepth < depth) {
-    maxDepth = depth;
-  }
+      depth++;
+      curNode = curNode.children[byte];
+    } // for each byte
 
-  if (curNode.mimeType) {
-    throw `File signature collision:  ${curNode.mimeType} overlaps with ${mimeType}`;
-  } else if (Object.keys(curNode.children).length > 0) {
-    throw `${mimeType} signature is not unique, it collides with other mime types`;
-  }
-  curNode.mimeType = mimeType;
+    if (maxDepth < depth) {
+      maxDepth = depth;
+    }
+
+    if (curNode.mimeType) {
+      throw `File signature collision:  ${curNode.mimeType} overlaps with ${mimeType}`;
+    } else if (Object.keys(curNode.children).length > 0) {
+      throw `${mimeType} signature is not unique, it collides with other mime types`;
+    }
+    curNode.mimeType = mimeType;
+  } // for each signature
 }
 
 /**
